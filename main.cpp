43863985d6e2cb96f7e3bd5de4fdaa436007e42a
Omifes <russ.out.338@gmail.com>
2024-12-17 16:46:02 +0300

    New main

--- main.cpp
+++ main.cpp
@@ -9,32 +9,37 @@ using namespace std;
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    vector<Shape*> shapes;
 
-    
-    Shape* ellipse = new Ellipse();
-    Shape* square = new Square();
-
-    
-    cout << "Введите данные для эллипса (x, y, вертикальный радиус,горизонтальный радиус): ";
-    cin >> dynamic_cast<Ellipse&>(*ellipse);
+    cout << "Введите данные для эллипса (x, y, вертикальный радиус, горизонтальный радиус): ";
+    Ellipse ellipse;
+    cin >> ellipse;
 
     cout << "Введите данные для квадрата (x, y, сторона): ";
-    cin >> dynamic_cast<Square&>(*square);
+    Square square;
+    cin >> square;
     
-    //Тестировка метода scale
-    
-    //Тестировка класса CompositeShape
     CompositeShape composite(2);
-    composite.addShape(ellipse);
-    composite.addShape(square);
-    cout << composite;
-    /*cout << composite.getFrameRectangle().getWidth() << '\n' << composite.getFrameRectangle().getHeight() << '\n';
-    cout << composite.getFrameRectangle().getPos().getX() << " " << composite.getFrameRectangle().getPos().getY() << '\n';*/
-    composite.scale(2);
+    composite.addShape(ellipse.clone());
+    composite.addShape(square.clone());
     
-    cout << "Изменения полсе изотропного масштабирования:\n\n"<< composite << '\n';
-    /*cout << composite.getFrameRectangle().getWidth() << '\n' << composite.getFrameRectangle().getHeight() << '\n';
-    cout << composite.getFrameRectangle().getPos().getX() << " " << composite.getFrameRectangle().getPos().getY() << '\n';*/
+    cout << "Начальные данные составной фигуры:\n";
+    cout << composite << '\n';
+
+    cout << "Введите координаты для перемещения составной фигуры в точку (x, y): ";
+    double moveX, moveY;
+    cin >> moveX >> moveY;
+    composite.moveToPoint(Point(moveX, moveY));
+
+    cout << "После перемещения составной фигуры:\n";
+    cout << composite << '\n';
+
+    cout << "Введите коэффициент масштабирования: ";
+    double scaleFactor;
+    cin >> scaleFactor;
+    composite.scale(scaleFactor);
+
+    cout << "После масштабирования составной фигуры:\n";
+    cout << composite << '\n';
+
     return 0;
 }
